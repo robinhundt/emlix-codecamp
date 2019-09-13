dab19adb2042e593a587d012c4c6affca31a2e07
Thilo Wischmeyer <thwischm@gmail.com>
2019-09-13 14:16:53 +0200

    add iptables setup to start script

--- start.sh
+++ start.sh
@@ -5,6 +5,8 @@ cd $(dirname "$0")
 insmod driver/i2c-bme280.ko
 echo "bme280 0x76" > /sys/bus/i2c/devices/i2c-1/new_device
 
+iptables -A INPUT -p tcp -m multiport --dports 80,443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp -m multiport --dports 80,443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 server/server.py &
 ambient-light/ambient-light.py -c
 
