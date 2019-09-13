c578409b4e5bcc803aa52b8201572666a9c58d10
Thilo Wischmeyer <thwischm@gmail.com>
2019-09-13 13:46:04 +0200

    stop background processes on exit

--- start.sh
+++ start.sh
@@ -5,5 +5,7 @@ cd $(dirname "$0")
 insmod driver/i2c-bme280.ko
 echo "bme280 0x76" > /sys/bus/i2c/devices/i2c-1/new_device
 
-ambient-light/ambient-light.py &
-server/server.py
+server/server.py &
+ambient-light/ambient-light.py -c
+
+trap "kill 0" EXIT
